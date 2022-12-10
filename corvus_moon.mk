3ce6d63d18f7e600348622e766ab80c386c66dec
Garten <brilliangarten@gmail.com>
2022-12-10 23:30:40 +0700

    Update and rename rice_moon.mk to corvus_moon.mk

--- corvus_moon.mk
+++ corvus_moon.mk
@@ -21,10 +21,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/moon/device.mk)
 # Inherit some rom sruff
-$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, vendor/corvus/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := rice_moon
+PRODUCT_NAME := corvus_moon
 PRODUCT_DEVICE := moon
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme Moon
@@ -42,21 +42,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-realme
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
-
-# maintainer flag (one word only/no spacing)
-RICE_MAINTAINER := Garten
-
-# chipset flag (one word only/no spacing)
-RICE_CHIPSET := HelioG85
-
-# Sushi Bootanimation (only 720/1080p/1440 supported. if not defined, bootanimation is google bootanimation)
-SUSHI_BOOTANIMATION := 720
-
-# Graphene Camera
-TARGET_BUILD_GRAPHENEOS_CAMERA := true
-
-# disable/enable blur support, default is false
-TARGET_ENABLE_BLUR := true
-
-# Face Unlock
-TARGET_FACE_UNLOCK_SUPPORTED := true
